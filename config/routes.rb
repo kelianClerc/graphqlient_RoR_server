ed3aaf35e7d43eee39301d6e4f7ac3cbf6e7b199
Kélian Clerc <kelian.clerc@applidium.com>
2017-07-20 11:37:28 +0200

    Add config files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+
+  post "/graphql", to: "graphql#execute"
+
+  get "/graphql/test"
+  root "graphql#test"
+
+
   resources :users do
     resources :actions
   end
+
+
+  resources :queries, via: [:post, :options]
 end
